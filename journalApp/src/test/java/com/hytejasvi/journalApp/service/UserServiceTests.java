89ff7a138552c1a882c81755ad99a97bc387ae3c
hytejasvi <hytejasvi@gmail.com>
2024-10-18 11:02:02 +0200

    a

--- journalApp/src/test/java/com/hytejasvi/journalApp/service/UserServiceTests.java
+++ journalApp/src/test/java/com/hytejasvi/journalApp/service/UserServiceTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Disabled
 @SpringBootTest
 public class UserServiceTests {
 
