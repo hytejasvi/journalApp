157acb8f1ac47695b6723dd756a7eb0efc616054
hytejasvi <hytejasvi@gmail.com>
2024-11-04 19:06:39 +0100

    changing the create user end point to signup and login and add jwt based authentication

--- journalApp/src/main/java/com/hytejasvi/journalApp/controller/PublicController.java
+++ journalApp/src/main/java/com/hytejasvi/journalApp/controller/PublicController.java
@@ -2,23 +2,55 @@ package com.hytejasvi.journalApp.controller;
 
 import com.hytejasvi.journalApp.Entity.User;
 import com.hytejasvi.journalApp.service.UserService;
+import com.hytejasvi.journalApp.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
 
     @Autowired
     UserService userService;
-    @PostMapping("/create-user")
-    public ResponseEntity<?> createUser(@RequestBody User user) {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user) {
         if (userService.createUser(user)) {
             return new ResponseEntity<>(HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>("UserName Already Exists", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwtToken = jwtUtil.generateTokens(userDetails.getUsername());
+            log.info("jwtToken: {}",jwtToken);
+            return new ResponseEntity<>(jwtToken, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>("Incorrect Username or Password", HttpStatus.BAD_REQUEST);
         }
     }
 }
