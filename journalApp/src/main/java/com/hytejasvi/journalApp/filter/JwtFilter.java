ffb4c74a5676cca8bc1e15b5eacd140f4b9c2701
hytejasvi <hytejasvi@gmail.com>
2024-11-04 19:12:54 +0100

    the JwtFilter class overrides the doFilterInternal where we perform Jwt related authorization, then we set the auth to the SecurityContext for further uses and then chain the doFilters for further filter chaining

---
+++ journalApp/src/main/java/com/hytejasvi/journalApp/filter/JwtFilter.java
@@ -0,0 +1,50 @@
+package com.hytejasvi.journalApp.filter;
+
+import com.hytejasvi.journalApp.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        String userName = null;
+        String jwt = null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            userName = jwtUtil.extractUserName(jwt);
+        }
+        if (userName != null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            if (JwtUtil.validToken(jwt)) {
+                UsernamePasswordAuthenticationToken auth = new
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        response.addHeader("userName", userName);
+        chain.doFilter(request,response);
+    }
+}
